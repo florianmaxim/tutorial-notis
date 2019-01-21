89e0c845b7311dafa73f56611ba3e6ac647d738f
florianmaxim <hello@florianmaxim>
2019-01-21 18:39:08 +0100

    hi

---
+++ tutorial-notification/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Permissions, Notifications} from 'expo';
+
+async function register(){
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+
+  console.log(token)
+}
+export default class App extends React.Component {
+
+  state = {
+    notification: {},
+  };
+
+  componentWillMount(){
+    register()
+  }
+
+  _handleNotification = (notification) => {
+    this.setState({notification: notification});
+    console.log(JSON.stringify(notification))
+  };
+
+  componentDidMount(){
+  
+      // Handle notifications that are received or selected while the app
+      // is open. If the app was closed and then opened by tapping the
+      // notification (rather than just tapping the app icon to open it),
+      // this function will fire on the next tick after the app starts
+      // with the notification data.
+      this._notificationSubscription = Notifications.addListener(this._handleNotification);
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Origin: {this.state.notification.origin}</Text>
+        <Text>Data: {JSON.stringify(this.state.notification.data)}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
